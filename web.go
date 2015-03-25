38a754f3c005f516669441c70347d2dc6a34ca44
Johannes Gilger <heipei@hackvalue.de>
2015-03-25 22:26:17 +0100

    TODO, learning

--- web.go
+++ web.go
@@ -9,7 +9,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
+
+var config struct {
+	pubkey  interface{}
+	privkey *ecdsa.PrivateKey
+}
 
 func readEcPublicKeyPem(filename string) (interface{}, error) {
 	dat, err := ioutil.ReadFile(filename)
@@ -17,12 +23,12 @@ func readEcPublicKeyPem(filename string) (interface{}, error) {
 
 	pemblock, _ := pem.Decode(dat)
 
-	pubkey, err := x509.ParsePKIXPublicKey(pemblock.Bytes)
+	config.pubkey, err = x509.ParsePKIXPublicKey(pemblock.Bytes)
 	check(err)
 
-	fmt.Println(pubkey)
+	fmt.Println(config.pubkey)
 
-	return pubkey, err
+	return config.pubkey, err
 }
 
 func readEcPrivateKeyPem(filename string) (*ecdsa.PrivateKey, error) {
@@ -31,10 +37,10 @@ func readEcPrivateKeyPem(filename string) (*ecdsa.PrivateKey, error) {
 
 	pemblock, _ := pem.Decode(dat)
 
-	privkey, err := x509.ParseECPrivateKey(pemblock.Bytes)
+	config.privkey, err = x509.ParseECPrivateKey(pemblock.Bytes)
 	check(err)
 
-	bytes, err := x509.MarshalECPrivateKey(privkey)
+	bytes, err := x509.MarshalECPrivateKey(config.privkey)
 	check(err)
 
 	block := pem.Block{}
@@ -43,9 +49,9 @@ func readEcPrivateKeyPem(filename string) (*ecdsa.PrivateKey, error) {
 	bytes_encoded := pem.EncodeToMemory(&block)
 	fmt.Println(string(bytes_encoded))
 
-	pubkey := privkey.Public()
+	config.pubkey = config.privkey.Public()
 
-	bytes, _ = x509.MarshalPKIXPublicKey(pubkey)
+	bytes, _ = x509.MarshalPKIXPublicKey(config.pubkey)
 	block = pem.Block{}
 	block.Type = "EC PUBLIC KEY"
 
@@ -54,11 +60,39 @@ func readEcPrivateKeyPem(filename string) (*ecdsa.PrivateKey, error) {
 
 	fmt.Println(string(bytes_encoded))
 
-	return privkey, err
+	return config.privkey, err
+}
+
+func login_handler(w http.ResponseWriter, r *http.Request) {
+	// This is how you get request headers
+	if val, ok := r.Header["User-Agent"]; ok {
+		fmt.Printf("%s\n", val[0])
+	}
+
+	// Get the remote address
+	fmt.Printf(">> New login request from %s\n", r.RemoteAddr)
+
+	// Iterate over all headers
+	// Join strings
+	for key, value := range r.Header {
+		fmt.Printf("%s: %s\n", key, strings.Join(value, ""))
+	}
+
+	// This is how you set response headers
+	w.Header().Set("Custom-Header", "Jojo")
+
+	// This is how you set the status code and return immediately
+	// w.WriteHeader(404)
+
+	fmt.Fprintf(w, "%#v\n", config.pubkey)
+	fmt.Fprintf(w, "You have been logged in!\n")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	http.Error(w, "Error xyz", http.StatusUnauthorized)
+	return
+	fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
+	fmt.Fprintf(w, "Test123!\n")
 }
 
 func check(e error) {
@@ -69,6 +103,8 @@ func check(e error) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", login_handler)
+
 	_, err := readEcPrivateKeyPem("prime256v1-key.pem")
 	check(err)
 
